e9e086233a5e289c637c779e7c16055951fe039d
fajartc02 <fajartc02@gmail.com>
2025-01-28 00:01:55 +0700

    update features

--- controllers/problems.js
+++ controllers/problems.js
@@ -7,202 +7,275 @@ const { v4: uuidv4 } = require("uuid");
 const Line = require("../models/line");
 
 module.exports = {
-  getAllProblems: async (req, res) => {
-    try {
-      req.query.yearMonth = req.query.yearMonth || moment().format("YYYY-MM");
+    getAllProblems: async (req, res) => {
+        try {
+            req.query.yearMonth = req.query.yearMonth || moment().format("YYYY-MM");
 
-      const problems = await Problem.getAll(req.query);
+            const problems = await Problem.getAll(req.query);
 
-      responseStatus.common(res, problems);
-    } catch (error) {
-      console.log(error);
-      responseStatus.serverError(res, error);
-    }
-  },
-  getGraphProblem: async (req, res) => {
-    try {
-      req.query.yearMonth = req.query.yearMonth || moment().format("YYYY-MM");
-      req.query.groupBy = req.query.groupBy || "overall";
-      req.query.categoryId = req.query.categoryId || null;
-      const problems = await Problem.getAll(req.query);
-
-      if (req.query.groupBy == "shop") {
-        const shops = await Shop.getAll();
-        let responseData = {
-          series: [],
-          stacked: false,
-          colors: [],
-          categories: ["W1", "W2", "W3", "W4"],
-          legends: [],
-        };
-
-        let shopSeriesMap = shops.map((shop) => {
-          responseData.colors.push(shop.color);
-          responseData.legends.push({
-            name: shop.sname,
-            color: shop.color,
-          });
-          let container = [0, 0, 0, 0];
-          for (let i = 0; i < problems.length; i++) {
-            const element = problems[i];
-            let idxPosition = Number(element.week_id.slice(1)) - 1;
-            if (element.shop_id == shop.id) {
-              container[idxPosition] += 1;
+            responseStatus.common(res, problems);
+        } catch (error) {
+            console.log(error);
+            responseStatus.serverError(res, error);
+        }
+    },
+    getGraphProblem: async (req, res) => {
+        try {
+            req.query.yearMonth = req.query.yearMonth || moment().format("YYYY-MM");
+            req.query.groupBy = req.query.groupBy || "overall";
+            req.query.categoryId = req.query.categoryId || null;
+            let problems = await Problem.getAll(req.query);
+            if (req.query.groupBy === "yearly") {
+                problems = await Problem.getAllYearly(req.query);
             }
-          }
 
-          let obj = {
-            name: shop.sname,
-            data: container,
-          };
-          return obj;
-        });
+            if (req.query.groupBy == "shop") {
+                const shops = await Shop.getAll();
+                let responseData = {
+                    series: [],
+                    stacked: false,
+                    colors: [],
+                    categories: ["W1", "W2", "W3", "W4"],
+                    legends: [],
+                };
 
-        responseData.series = shopSeriesMap;
-        responseStatus.common(res, responseData);
-      } else if (req.query.groupBy == "department") {
-        const departments = await Department.getAll();
-        let responseData = {
-          series: [],
-          stacked: true,
-          colors: [],
-          categories: ["W1", "W2", "W3", "W4"],
-          legends: [],
-        };
+                let shopSeriesMap = shops.map((shop) => {
+                    responseData.colors.push(shop.color);
+                    responseData.legends.push({
+                        name: shop.sname,
+                        color: shop.color,
+                    });
+                    let container = [0, 0, 0, 0];
+                    for (let i = 0; i < problems.length; i++) {
+                        const element = problems[i];
+                        let idxPosition = Number(element.week_id.slice(1)) - 1;
+                        if (element.shop_id == shop.id) {
+                            container[idxPosition] += 1;
+                        }
+                    }
 
-        let departmentSeriesMap = departments.map((department) => {
-          responseData.colors.push(department.color);
-          responseData.legends.push({
-            name: department.sname,
-            color: department.color,
-          });
-          let container = [0, 0, 0, 0];
-          for (let i = 0; i < problems.length; i++) {
-            const element = problems[i];
-            let idxPosition = Number(element.week_id.slice(1)) - 1;
-            if (element.department_id == department.id) {
-              container[idxPosition] += 1;
+                    let obj = {
+                        name: shop.sname,
+                        data: container,
+                    };
+                    return obj;
+                });
+
+                responseData.series = shopSeriesMap;
+                responseStatus.common(res, responseData);
+            } else if (req.query.groupBy == "department") {
+                const departments = await Department.getAll();
+                let responseData = {
+                    series: [],
+                    stacked: true,
+                    colors: [],
+                    categories: ["W1", "W2", "W3", "W4"],
+                    legends: [],
+                };
+
+                let departmentSeriesMap = departments.map((department) => {
+                    responseData.colors.push(department.color);
+                    responseData.legends.push({
+                        name: department.sname,
+                        color: department.color,
+                    });
+                    let container = [0, 0, 0, 0];
+                    for (let i = 0; i < problems.length; i++) {
+                        const element = problems[i];
+                        let idxPosition = Number(element.week_id.slice(1)) - 1;
+                        if (element.department_id == department.id) {
+                            container[idxPosition] += 1;
+                        }
+                    }
+
+                    let obj = {
+                        name: department.sname,
+                        data: container,
+                    };
+                    return obj;
+                });
+
+                responseData.series = departmentSeriesMap;
+                responseStatus.common(res, responseData);
+            } else if (req.query.groupBy == "overall") {
+                let responseData = {
+                    series: [{
+                        name: "Closed",
+                        data: [0, 0, 0, 0],
+                    },
+                    {
+                        name: "On Progress",
+                        data: [0, 0, 0, 0],
+                    },
+                    {
+                        name: "Delay",
+                        data: [0, 0, 0, 0],
+                    },
+                    ],
+                    stacked: true,
+                    colors: ["#4FAD5B", "#F5C242", "#EA3323"],
+                    categories: ["W1", "W2", "W3", "W4"],
+                    legends: [],
+                };
+                responseData.legends = [{
+                    name: "Closed",
+                    color: "#4FAD5B",
+                },
+                {
+                    name: "On Progress",
+                    color: "#F5C242",
+                },
+                {
+                    name: "Delay",
+                    color: "#EA3323",
+                },
+                ];
+
+                responseData.series.map((serie) => {
+                    for (let i = 0; i < problems.length; i++) {
+                        const problem = problems[i];
+                        let conditionClosed = problem.status_id == 4;
+
+                        let isDelay =
+                            moment(problem.countermeasure_date).diff(moment(), "day") < 0;
+
+                        let idxPosition = Number(problem.week_id.slice(1)) - 1;
+                        if (serie.name == "Closed" && conditionClosed) {
+                            serie.data[idxPosition] += 1;
+                        } else if (
+                            serie.name == "On Progress" &&
+                            !conditionClosed &&
+                            !isDelay
+                        ) {
+                            serie.data[idxPosition] += 1;
+                        } else if (serie.name == "Delay" && isDelay && !conditionClosed) {
+                            serie.data[idxPosition] += 1;
+                        }
+                    }
+                    return serie;
+                });
+
+                responseStatus.common(res, responseData);
+            } else if (req.query.groupBy == "yearly") {
+                let responseData = {
+                    series: [{
+                        name: "Closed",
+                        data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    },
+                    {
+                        name: "On Progress",
+                        data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    },
+                    {
+                        name: "Delay",
+                        data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    },
+                    ],
+                    stacked: true,
+                    colors: ["#4FAD5B", "#F5C242", "#EA3323"],
+                    categories: [
+                        "Jan",
+                        "Feb",
+                        "Mar",
+                        "Apr",
+                        "May",
+                        "Jun",
+                        "Jul",
+                        "Aug",
+                        "Sep",
+                        "Oct",
+                        "Nov",
+                        "Dec",
+                    ],
+                    legends: [],
+                };
+                responseData.legends = [{
+                    name: "Closed",
+                    color: "#4FAD5B",
+                },
+                {
+                    name: "On Progress",
+                    color: "#F5C242",
+                },
+                {
+                    name: "Delay",
+                    color: "#EA3323",
+                },
+                ];
+
+                responseData.series.map((serie) => {
+                    for (let i = 0; i < problems.length; i++) {
+                        const problem = problems[i];
+                        let conditionClosed = problem.status_id == 4;
+
+                        let isDelay =
+                            moment(problem.countermeasure_date).diff(moment(), "day") < 0;
+
+                        // let idxPosition = Number(problem.week_id.slice(1)) - 1;
+                        // if (serie.name == "Closed" && conditionClosed) {
+                        //     serie.data[idxPosition] += 1;
+                        // } else if (
+                        //     serie.name == "On Progress" &&
+                        //     !conditionClosed &&
+                        //     !isDelay
+                        // ) {
+                        //     serie.data[idxPosition] += 1;
+                        // } else if (serie.name == "Delay" && isDelay && !conditionClosed) {
+                        //     serie.data[idxPosition] += 1;
+                        // }
+                    }
+                    return serie;
+                });
+
+                responseStatus.common(res, responseData);
             }
-          }
+        } catch (error) {
+            console.log(error);
+            responseStatus.serverError(res, error);
+        }
+    },
+    getProblemById: async (req, res) => {
+        try {
+            const problem = await Problem.getById(req.params.id);
+            responseStatus.common(res, problem);
+        } catch (error) {
+            console.log(error);
+            responseStatus.serverError(res, error);
+        }
+    },
+    addProblem: async (req, res) => {
+        try {
+            req.body.uuid = uuidv4();
+            console.log(req.body);
 
-          let obj = {
-            name: department.sname,
-            data: container,
-          };
-          return obj;
-        });
+            const selectedLine = await Line.getById(req.body.line_id);
+            req.body.shop_id = selectedLine.shop_id;
 
-        responseData.series = departmentSeriesMap;
-        responseStatus.common(res, responseData);
-      } else if (req.query.groupBy == "overall") {
-        let responseData = {
-          series: [
-            {
-              name: "Closed",
-              data: [0, 0, 0, 0],
-            },
-            {
-              name: "On Progress",
-              data: [0, 0, 0, 0],
-            },
-            {
-              name: "Delay",
-              data: [0, 0, 0, 0],
-            },
-          ],
-          stacked: true,
-          colors: ["#4FAD5B", "#F5C242", "#EA3323"],
-          categories: ["W1", "W2", "W3", "W4"],
-          legends: [],
-        };
-        responseData.legends = [
-          {
-            name: "Closed",
-            color: "#4FAD5B",
-          },
-          {
-            name: "On Progress",
-            color: "#F5C242",
-          },
-          {
-            name: "Delay",
-            color: "#EA3323",
-          },
-        ];
+            await Problem.create(req.body);
+            responseStatus.common(res, "ok");
+        } catch (error) {
+            console.log(error);
+            responseStatus.serverError(res, error);
+        }
+    },
+    updateProblem: async (req, res) => {
+        try {
+            console.log(req.body);
 
-        responseData.series.map((serie) => {
-          for (let i = 0; i < problems.length; i++) {
-            const problem = problems[i];
-            let conditionClosed = problem.status_id == 4;
-
-            let isDelay =
-              moment(problem.countermeasure_date).diff(moment(), "day") < 0;
-
-            let idxPosition = Number(problem.week_id.slice(1)) - 1;
-            if (serie.name == "Closed" && conditionClosed) {
-              serie.data[idxPosition] += 1;
-            } else if (
-              serie.name == "On Progress" &&
-              !conditionClosed &&
-              !isDelay
-            ) {
-              serie.data[idxPosition] += 1;
-            } else if (serie.name == "Delay" && isDelay && !conditionClosed) {
-              serie.data[idxPosition] += 1;
-            }
-          }
-          return serie;
-        });
-
-        responseStatus.common(res, responseData);
-      }
-    } catch (error) {
-      console.log(error);
-      responseStatus.serverError(res, error);
-    }
-  },
-  getProblemById: async (req, res) => {
-    try {
-      const problem = await Problem.getById(req.params.id);
-      responseStatus.common(res, problem);
-    } catch (error) {
-      console.log(error);
-      responseStatus.serverError(res, error);
-    }
-  },
-  addProblem: async (req, res) => {
-    try {
-      req.body.uuid = uuidv4();
-      console.log(req.body);
-
-      const selectedLine = await Line.getById(req.body.line_id);
-      req.body.shop_id = selectedLine.shop_id;
-
-      await Problem.create(req.body);
-      responseStatus.common(res, "ok");
-    } catch (error) {
-      console.log(error);
-      responseStatus.serverError(res, error);
-    }
-  },
-  updateProblem: async (req, res) => {
-    try {
-      console.log(req.body);
-
-      await Problem.update(req.params.id, req.body);
-      responseStatus.common(res, "ok");
-    } catch (error) {
-      console.log(error);
-      responseStatus.serverError(res, error);
-    }
-  },
-  removeProblem: async (req, res) => {
-    try {
-      await Problem.delete(req.params.id);
-      responseStatus.common(res, "ok");
-    } catch (error) {
-      console.log(error);
-      responseStatus.serverError(res, error);
-    }
-  },
+            await Problem.update(req.params.id, req.body);
+            responseStatus.common(res, "ok");
+        } catch (error) {
+            console.log(error);
+            responseStatus.serverError(res, error);
+        }
+    },
+    removeProblem: async (req, res) => {
+        try {
+            await Problem.delete(req.params.id);
+            responseStatus.common(res, "ok");
+        } catch (error) {
+            console.log(error);
+            responseStatus.serverError(res, error);
+        }
+    },
 };
--- models/problem.js
+++ models/problem.js
@@ -5,138 +5,188 @@ const tableName = "tb_r_problems";
 const moment = require("moment");
 
 class Problem {
-  constructor(columns = `${tableName}.*`) {
-    this.columns = columns;
-  }
+    constructor(columns = `${tableName}.*`) {
+        this.columns = columns;
+    }
 
-  static async getAll({
-    page = null,
-    pageSize = null,
-    yearMonth,
-    categoryId = null,
-  }) {
-    console.log(page, pageSize, yearMonth, categoryId);
-    let query = {};
-    const startOfMonth = moment(yearMonth)
-      .startOf("month")
-      .format("YYYY-MM-DD");
-    const endOfMonth = moment(yearMonth).endOf("month").format("YYYY-MM-DD");
+    static async getAll({
+        page = null,
+        pageSize = null,
+        yearMonth,
+        categoryId = null,
+    }) {
+        let query = {};
+        const startOfMonth = moment(yearMonth)
+            .startOf("month")
+            .format("YYYY-MM-DD");
+        const endOfMonth = moment(yearMonth).endOf("month").format("YYYY-MM-DD");
 
-    categoryId ? (query.category_id = categoryId) : null;
-    return db(tableName)
-      .join("tb_m_lines", `${tableName}.line_id`, "=", "tb_m_lines.id")
-      .join("tb_m_shifts", `${tableName}.shift_id`, "=", "tb_m_shifts.id")
-      .join(
-        "tb_m_departments",
-        `${tableName}.department_id`,
-        "=",
-        "tb_m_departments.id"
-      )
-      .join("tb_m_statuses", `${tableName}.status_id`, "=", "tb_m_statuses.id")
-      .join(
-        "tb_m_categories",
-        `${tableName}.category_id`,
-        "=",
-        "tb_m_categories.id"
-      )
-      .rowNumber("no", function () {
-        this.orderBy(`${tableName}.id`);
-      })
-      .select(
-        `${tableName}.*`,
-        db.raw(`CASE
+        categoryId ? (query.category_id = categoryId) : null;
+        return db(tableName)
+            .join("tb_m_lines", `${tableName}.line_id`, "=", "tb_m_lines.id")
+            .join("tb_m_shifts", `${tableName}.shift_id`, "=", "tb_m_shifts.id")
+            .join(
+                "tb_m_departments",
+                `${tableName}.department_id`,
+                "=",
+                "tb_m_departments.id"
+            )
+            .join("tb_m_statuses", `${tableName}.status_id`, "=", "tb_m_statuses.id")
+            .join(
+                "tb_m_categories",
+                `${tableName}.category_id`,
+                "=",
+                "tb_m_categories.id"
+            )
+            .rowNumber("no", function () {
+                this.orderBy(`${tableName}.id`);
+            })
+            .select(
+                `${tableName}.*`,
+                db.raw(`CASE
           WHEN EXTRACT(DAY FROM problem_date) BETWEEN 1 AND 7 THEN 'W1'
           WHEN EXTRACT(DAY FROM problem_date) BETWEEN 8 AND 14 THEN 'W2'
           WHEN EXTRACT(DAY FROM problem_date) BETWEEN 15 AND 21 THEN 'W3'
           ELSE 'W4'
         END AS week_id`),
-        `tb_m_lines.sname as line_nm`,
-        `tb_m_shifts.name as shift_nm`,
-        `tb_m_departments.sname as department_nm`,
-        `tb_m_statuses.name as status_nm`,
-        `tb_m_categories.name as category_nm`,
-        `tb_m_statuses.img as img`
-      )
-      .whereBetween("problem_date", [`${startOfMonth}`, `${endOfMonth}`])
-      .where(query)
-      .limit(pageSize || 10)
-      .offset(((page || 1) - 1) * (pageSize || 10))
-      .orderBy("problem_date", "asc");
-  }
+                `tb_m_lines.sname as line_nm`,
+                `tb_m_shifts.name as shift_nm`,
+                `tb_m_departments.sname as department_nm`,
+                `tb_m_statuses.name as status_nm`,
+                `tb_m_categories.name as category_nm`,
+                `tb_m_statuses.img as img`
+            )
+            .whereBetween("problem_date", [`${startOfMonth}`, `${endOfMonth}`])
+            .where(query)
+            .limit(pageSize || 1000)
+            .offset(((page || 1) - 1) * (pageSize || 10))
+            .orderBy("problem_date", "asc");
+    }
 
-  static async getGraph({ yearMonth }) {
-    const result = await db(tableName)
-      .join("tb_m_lines", `${tableName}.line_id`, "=", "tb_m_lines.id")
-      .join("tb_m_shops", `${"tb_m_lines"}.shop_id`, "=", "tb_m_shops.id")
-      .select(
-        db.raw(`CASE
+    static async getAllYearly({
+        page = null,
+        pageSize = null,
+        yearMonth,
+        categoryId = null,
+    }) {
+        let query = {};
+        const startOfMonth = moment(yearMonth).startOf("year").format("YYYY-MM-DD");
+        const endOfMonth = moment(yearMonth).endOf("year").format("YYYY-MM-DD");
+        console.log(startOfMonth, endOfMonth);
+
+        categoryId ? (query.category_id = categoryId) : null;
+        return db(tableName)
+            .join("tb_m_lines", `${tableName}.line_id`, "=", "tb_m_lines.id")
+            .join("tb_m_shifts", `${tableName}.shift_id`, "=", "tb_m_shifts.id")
+            .join(
+                "tb_m_departments",
+                `${tableName}.department_id`,
+                "=",
+                "tb_m_departments.id"
+            )
+            .join("tb_m_statuses", `${tableName}.status_id`, "=", "tb_m_statuses.id")
+            .join(
+                "tb_m_categories",
+                `${tableName}.category_id`,
+                "=",
+                "tb_m_categories.id"
+            )
+            .rowNumber("no", function () {
+                this.orderBy(`${tableName}.id`);
+            })
+            .select(
+                `${tableName}.*`,
+                `tb_m_lines.sname as line_nm`,
+                `tb_m_shifts.name as shift_nm`,
+                `tb_m_departments.sname as department_nm`,
+                `tb_m_statuses.name as status_nm`,
+                `tb_m_categories.name as category_nm`,
+                `tb_m_statuses.img as img`
+            )
+            .whereBetween("problem_date", [`${startOfMonth}`, `${endOfMonth}`])
+            .where(query)
+            .limit(pageSize || 1000)
+            .offset(((page || 1) - 1) * (pageSize || 10))
+            .orderBy("problem_date", "asc");
+    }
+
+    static async getGraph({ yearMonth }) {
+        const result = await db(tableName)
+            .join("tb_m_lines", `${tableName}.line_id`, "=", "tb_m_lines.id")
+            .join("tb_m_shops", `${"tb_m_lines"}.shop_id`, "=", "tb_m_shops.id")
+            .select(
+                db.raw(`CASE
           WHEN EXTRACT(DAY FROM problem_date) BETWEEN 1 AND 7 THEN 'W1'
           WHEN EXTRACT(DAY FROM problem_date) BETWEEN 8 AND 14 THEN 'W2'
           WHEN EXTRACT(DAY FROM problem_date) BETWEEN 15 AND 21 THEN 'W3'
           ELSE 'W4'
         END AS week`),
-        db.raw(`COUNT(${tableName}.id) AS total_problems`),
-        `${"tb_m_shops"}.id AS shop_id`
-      )
-      .whereRaw("EXTRACT(MONTH FROM problem_date) = 8") // Filter for August (month 8)
-      .groupBy("week", `${"tb_m_shops"}.id `)
-      .orderBy(["week", "shop_id"]);
-    // console.log(result);
+                db.raw(`COUNT(${tableName}.id) AS total_problems`),
+                `${"tb_m_shops"}.id AS shop_id`
+            )
+            .whereRaw("EXTRACT(MONTH FROM problem_date) = 8") // Filter for August (month 8)
+            .groupBy("week", `${"tb_m_shops"}.id `)
+            .orderBy(["week", "shop_id"]);
+        // console.log(result);
 
-    // const dataCte = db(tableName)
-    //   .join("tb_m_lines", `${tableName}.line_id`, "=", "tb_m_lines.id")
-    //   .join("tb_m_shifts", `${tableName}.shift_id`, "=", "tb_m_shifts.id")
-    //   .join(
-    //     "tb_m_departments",
-    //     `${tableName}.department_id`,
-    //     "=",
-    //     "tb_m_departments.id"
-    //   )
-    //   .join("tb_m_statuses", `${tableName}.status_id`, "=", "tb_m_statuses.id")
-    //   .join(
-    //     "tb_m_categories",
-    //     `${tableName}.category_id`,
-    //     "=",
-    //     "tb_m_categories.id"
-    //   )
-    //   .join("tb_m_shops", `tb_m_lines.shop_id`, "=", "tb_m_shops.id")
+        // const dataCte = db(tableName)
+        //   .join("tb_m_lines", `${tableName}.line_id`, "=", "tb_m_lines.id")
+        //   .join("tb_m_shifts", `${tableName}.shift_id`, "=", "tb_m_shifts.id")
+        //   .join(
+        //     "tb_m_departments",
+        //     `${tableName}.department_id`,
+        //     "=",
+        //     "tb_m_departments.id"
+        //   )
+        //   .join("tb_m_statuses", `${tableName}.status_id`, "=", "tb_m_statuses.id")
+        //   .join(
+        //     "tb_m_categories",
+        //     `${tableName}.category_id`,
+        //     "=",
+        //     "tb_m_categories.id"
+        //   )
+        //   .join("tb_m_shops", `tb_m_lines.shop_id`, "=", "tb_m_shops.id")
 
-    //   .select(
-    //     `tb_m_shops.sname as shop_nm`
-    //     // `tb_m_departments.sname as deptartment_nm`
-    //   )
-    //   .count(`${tableName}.id as total`)
-    //   .groupBy(
-    //     "tb_m_shops.sname"
-    //     // "tb_m_departments.sname"
-    //   )
-    //   .whereBetween("problem_date", [`${yearMonth}-01`, `${yearMonth}-31`]);
-    // const data = db(tableName)
-    // .with("countProblem", dataCte)
-    // .select("*")
+        //   .select(
+        //     `tb_m_shops.sname as shop_nm`
+        //     // `tb_m_departments.sname as deptartment_nm`
+        //   )
+        //   .count(`${tableName}.id as total`)
+        //   .groupBy(
+        //     "tb_m_shops.sname"
+        //     // "tb_m_departments.sname"
+        //   )
+        //   .whereBetween("problem_date", [`${yearMonth}-01`, `${yearMonth}-31`]);
+        // const data = db(tableName)
+        // .with("countProblem", dataCte)
+        // .select("*")
 
-    //   // return dataCte;
-    // // const problem = db(tableName).with;
-  }
+        //   // return dataCte;
+        // // const problem = db(tableName).with;
+    }
 
-  static async getById(id) {
-    return db(tableName).where({ id }).first();
-  }
+    static async getById(id) {
+        return db(tableName).where({ id }).first();
+    }
 
-  static async create(payload) {
-    return db(tableName).insert(payload).returning(this.columns);
-  }
+    static async create(payload) {
+        const generateId = await db(tableName).max("id").first().then((res) => {
+            return res.max + 1;
+        });
+        payload.id = generateId;
+        return db(tableName).insert(payload).returning(this.columns);
+    }
 
-  static async update(id, updatedData) {
-    return db(tableName)
-      .where({ id })
-      .update(updatedData)
-      .returning(this.columns);
-  }
+    static async update(id, updatedData) {
+        return db(tableName)
+            .where({ id })
+            .update(updatedData)
+            .returning(this.columns);
+    }
 
-  static async delete(id) {
-    return db(tableName).where({ id }).del();
-  }
+    static async delete(id) {
+        return db(tableName).where({ id }).del();
+    }
 }
 
 module.exports = Problem;
